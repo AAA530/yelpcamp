d4b8915a3e9979d3f0cd6cffe475fe324e4e1da9
AAA530 <dhairyapatelad@gmail.com>
2020-03-22 22:50:59 +0530

    now going to add description

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 var express = require('express');
-var body_parser = require('body-parser'); 
-
+var body_parser = require('body-parser');
+var mongoose = require('mongoose'); 
 var app = express();
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+
 app.use(express.static('public')); // This line.
 app.use(body_parser.urlencoded({extended : true}))
 
 app.set("view engine","ejs");
 
-var camp = [
-    {name : "dhiarya" ,image : "https://images.unsplash.com/photo-1475778057357-d35f37fa89dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name : "krisha" ,image : "https://images.unsplash.com/photo-1475778057357-d35f37fa89dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name : "ankur" ,image : "https://images.unsplash.com/photo-1475778057357-d35f37fa89dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-]
+var campschema = new mongoose.Schema({
+    name : String,
+    image : String,
+    description : String
+});
+
+var camp = mongoose.model("camp", campschema);
 
 app.get('/',(req,res)=>{
     res.render("landing")
 })
 
 app.get('/campgrounds',(req,res)=>{
-    
-    res.render("campground",{campground : camp});
+    camp.find({},(err,camp)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campground",{campground : camp});
+        }
+    })
 });
 
 
@@ -29,13 +45,21 @@ app.post('/campgrounds',(req,res)=>{
     var image = req.body.image;
     var n = {name : name ,image:image}
     
-    camp.push(n);
-    console.log(name);
-    res.redirect('/campgrounds');
+    camp.create(n , (err,camp)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/campgrounds');
+        }
+    })
 });
 
-app.get('/campground/new',(req,res)=>{
+app.get('/campgrounds/new',(req,res)=>{
     res.render('new_camp');
+})
+
+app.get('/campgrounds/:id',(req,res)=>{
+    res.send("this id");
 })
 
 app.listen(3000,function(){
