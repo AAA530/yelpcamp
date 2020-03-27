398571f58b0e88b7b762a1e6137711a1f4b3031e
AAA530 <dhairyapatelad@gmail.com>
2020-03-27 16:55:26 +0530

    comments style added

--- seeds.js
+++ seeds.js
@@ -7,17 +7,17 @@ var data = [
     {
         name : "First qqqqqqqq",
         image : "https://images.unsplash.com/photo-1504822503375-0db8ca37e40a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        description : "A common description for all campground",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci dolore perferendis minima deleniti voluptatibus molestias, officiis a ea in deserunt, similique quia animi. Ipsam a sint eius, minus laudantium dignissimos enim harum repellendus, dolores, impedit aspernatur eaque mollitia! Amet laudantium rerum tempora rem distinctio sapiente nesciunt excepturi aut. Quidem nam dolorum alias minus molestiae fuga totam, explicabo laboriosam in ipsam vitae qui at impedit ullam eveniet! At atque dolor dolorem soluta? Facere recusandae placeat voluptatibus impedit? Dignissimos possimus distinctio, esse voluptatem quas expedita fuga velit laborum ipsa, quasi saepe quia! Accusamus repellendus mollitia debitis sit maxime in tenetur, voluptate odit?",
     },
     {
         name : "Second camp",
         image : "https://images.unsplash.com/photo-1474314170901-f351b68f544f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        description : "A common description for all campground",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci dolore perferendis minima deleniti voluptatibus molestias, officiis a ea in deserunt, similique quia animi. Ipsam a sint eius, minus laudantium dignissimos enim harum repellendus, dolores, impedit aspernatur eaque mollitia! Amet laudantium rerum tempora rem distinctio sapiente nesciunt excepturi aut. Quidem nam dolorum alias minus molestiae fuga totam, explicabo laboriosam in ipsam vitae qui at impedit ullam eveniet! At atque dolor dolorem soluta? Facere recusandae placeat voluptatibus impedit? Dignissimos possimus distinctio, esse voluptatem quas expedita fuga velit laborum ipsa, quasi saepe quia! Accusamus repellendus mollitia debitis sit maxime in tenetur, voluptate odit?",
     },
     {
         name : "Third camp",
         image : "https://images.unsplash.com/photo-1470165473874-023613603389?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        description : "A common description for all campground",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci dolore perferendis minima deleniti voluptatibus molestias, officiis a ea in deserunt, similique quia animi. Ipsam a sint eius, minus laudantium dignissimos enim harum repellendus, dolores, impedit aspernatur eaque mollitia! Amet laudantium rerum tempora rem distinctio sapiente nesciunt excepturi aut. Quidem nam dolorum alias minus molestiae fuga totam, explicabo laboriosam in ipsam vitae qui at impedit ullam eveniet! At atque dolor dolorem soluta? Facere recusandae placeat voluptatibus impedit? Dignissimos possimus distinctio, esse voluptatem quas expedita fuga velit laborum ipsa, quasi saepe quia! Accusamus repellendus mollitia debitis sit maxime in tenetur, voluptate odit?",
     }
 ]
 
